51865a65c44a91b1724efed7e5250c0d7d7ed7b9
andrew-perc <andyperciballi@gmail.com>
2026-02-02 17:59:43 -0500

    fixed CRUD routes

--- controllers/tracks.controller.js
+++ controllers/tracks.controller.js
@@ -1,3 +1,55 @@
+// Bring in the Track Model
+const Track = require("../models/track");
+// Set up routing with express
+const express = require("express");
+const router = express.Router();
+
+/*
+    HTTP Method	Controller	Response	URI	Use Case
+    POST	create	200	/tracks	Create a track
+    GET	index	200	/tracks	List tracks
+    GET	show	200	/tracks/:trackId	Get a single track
+    PUT	update	200	/tracks/:trackId	Update a track
+    DELETE	deleteTrack	204	/tracks/:trackId	Delete a track
+*/
+
+// POST /tracks
+router.post("/", async (req, res) => {
+  try {
+    // throw new Error("Danger Bill Robinson!")
+    const newTrack = await Track.create(req.body);
+    res.status(201).json({
+      message: `Track ${newTrack.title} created successfully`,
+      title: newTrack,
+    });
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
+
+// GET /tracks
+router.get("/", async (req, res) => {
+  try {
+    const tracks = await Track.find(); // ({}) // give me all the tracks
+    res.status(200).json(tracks);
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
+
+// GET /tracks/:trackId
+router.get("/:trackId", async (req, res) => {
+  try {
+    const foundTrack = await Track.findById(req.params.trackId);
+    // If no track is founds, lets just return our 404 error message
+    if (!foundTrack) return res.status(404).json({ message: "Track Not Found" });
+    // Now we know that we have a track, lets send it back
+    res.status(200).json({ foundTrack });
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
+
 
 // DELETE /tracks/:trackId
 router.delete("/:trackId", async (req, res) => {
