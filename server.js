746728d4aa2c46f3d91bb07c093dd808bb367f8b
fbossom <felicia.bossom@yahoo.com>
2026-02-02 16:33:25 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require("cors")
+
+
+const app = express()
